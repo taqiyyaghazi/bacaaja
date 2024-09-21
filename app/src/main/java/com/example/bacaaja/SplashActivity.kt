1a40cfcac61dd6ecf894f32c7b285b11c2702d50
taqiyyaghazi <taqiyyaghazi@gmail.com>
2024-09-21 21:56:35 +0700

    change data

--- app/src/main/java/com/example/bacaaja/SplashActivity.kt
+++ app/src/main/java/com/example/bacaaja/SplashActivity.kt
@@ -18,6 +18,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 5000)
+        }, 3000)
     }
 }
